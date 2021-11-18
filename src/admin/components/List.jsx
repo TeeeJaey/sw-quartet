0553d5bc0ee0fb076a4572086ab873d584c9e4af
Tejas Jadhav <tjadhav95@gmail.com>
2021-11-18 15:46:47 +0530

    task#6 delete cards from admin

--- src/admin/components/List.jsx
+++ src/admin/components/List.jsx
@@ -7,9 +7,21 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Person from '../../comparable/Person';
+import { useDispatch } from 'react-redux';
 
 export default function List({cards}) {
-    console.log(cards);
+    const dispatch = useDispatch();
+    
+    const deleteCard = (index) => {
+        const action = {type:"delete_card",payload:{index:index}};
+        dispatch(action);
+    };
+
+    const deleteAllCards = () => {
+        const action = {type:"delete_all"};
+        dispatch(action);
+    };
+
     return (
         <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -23,14 +35,20 @@ export default function List({cards}) {
                                 ' (' + Person.properties[property].unit + ')'}
                             </TableCell>
                         ))}
+                        <TableCell>
+                            <button className="btn btn-danger" onClick={() => deleteAllCards()}>Delete All</button>
+                        </TableCell>
                     </TableRow>
-                    {cards.map(card => (<TableRow>
+                    {cards.map((card,index) => (<TableRow>
                         <TableCell>{card.name}</TableCell>
                         {Object.keys(Person.properties).map((property) => (
                             <TableCell>
                                 {card[property]}
                             </TableCell>
                         ))}
+                        <TableCell>
+                            <button className="btn btn-danger" onClick={() => deleteCard(index)}>Delete</button>
+                        </TableCell>
                     </TableRow>))}
                 </TableHead>
                 <TableBody>
--- src/admin/reducers/admin.reducer.ts
+++ src/admin/reducers/admin.reducer.ts
@@ -7,6 +7,21 @@ export interface State {
 
 export default function(state: State = { cards: cards }, action: any): State {
   switch (action.type) {
+    
+    case "delete_all": {
+      const newState = {...state};
+      newState.cards = [];
+      return newState;
+    }
+
+    case "delete_card": {
+      const newCards = [...state.cards];
+      newCards.splice(action.payload.index, 1);
+      const newState = {...state};
+      newState.cards = newCards;
+      return newState;
+    }
+
     default:
       return state;
   }
